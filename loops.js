6d48b9fb0adf6393fae7e87637835dc831f64553
John Kalinauskas <jkalinauskas@live.com>
2018-03-26 16:14:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -26,7 +26,20 @@ return arr
 
 
 
+ var array= [];
+
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+      if (i === 1) {
+        array.push("I am 1 strange loop.")
+      }
+      else {
+        array.push(`I am ${i} strange loops.`)
+      }
+  }
+  return array;
  
+}
 
 
 
